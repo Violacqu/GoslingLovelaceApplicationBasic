0c124e3df8cda72a99b3838d108506039af739b4
Violacqu <alice2747@126.com>
2019-03-30 13:14:26 +0800

    new

---
+++ app/src/main/java/com/example/gosling/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gosling;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+
+    }
+}
